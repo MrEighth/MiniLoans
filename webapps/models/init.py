1ea8568a630bf68bd76604fe1e43dc2e0cc1be4f
wusevsliuli <1558372253@qq.com>
2018-07-26 22:49:03 +0800

    模型修改

--- webapps/models/init.py
+++ webapps/models/init.py
@@ -8,8 +8,23 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-def create_sqlite_session(echo=False):
-    conn_str = 'sqlite:///test.db'
+def create_sqlite_engine(db='test.db', echo=False):
+    conn_str = 'sqlite:///{0}'.format(db)
     engine = create_engine(conn_str, echo=echo)
+    return engine
+
+
+def create_mysql_engine(uid='root', pwd='password', host='localhost', port='3306', db='test', echo=False):
+    conn_str = 'mysql+pymysql://{0}:{1}@{2}:{3}/{4}'.format(uid, pwd, host, port, db)
+    engine = create_engine(conn_str, echo=echo)
+    return engine
+
+
+def get_session(engine):
     db_session = sessionmaker(bind=engine)
     return db_session()
+
+
+if __name__ == '__main__':
+    engine = create_sqlite_engine()
+    session = get_session(engine)
--- webapps/models/models.py
+++ webapps/models/models.py
@@ -2,59 +2,89 @@
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy.types import Integer, String, Boolean, Float, CHAR, DateTime, CLOB
 from sqlalchemy.orm import sessionmaker, create_session, relationships, relationship
-from sqlalchemy.engine import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from webapps.models.init import create_sqlite_session
-Base = declarative_base()
-session = create_sqlite_session()
+from webapps.models.init import create_mysql_engine, get_session, Base
+from datetime import date, datetime
+engine = create_mysql_engine()
+session = get_session(engine)
+
+# 权限编码
+KH = 0b1000     # 客户
+KF = 0b0100     # 客服
+JL = 0b0010     # 经理
+GM = 0b0001     # 管理员
+
+# 用户操作类别
+ZC = 0b0001     # 注册
+RZ = 0b0010     # 实名认证
+SQ = 0b1000     # 借贷申请
+SP = 0b0100     # 审批
+
+# 资金流操作类别
+HK = 0b0001     # 还款
+JK = 0b0010     # 借款
+
 
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    uid = Column(CHAR(20), unique=True)
+    uid = Column(CHAR(10), unique=True)
     name = Column(String(20))
     sex = Column(String(20))
     phone_number = Column(CHAR(11), unique=True)
     ID_card = Column(CHAR(18), unique=True)
     wx_id = Column(String(50), unique=True)
     email = Column(String(50), unique=True)
-    credit = Column(Integer)
-    credit_data = Column(CLOB)
-    ceiling = Column(Float)
-    owed = Column(Float)
-    address = Column(String(100))
+    credit = Column(Integer, default=0)
+    # credit_data = Column(CLOB)
+    ceiling = Column(Float, default=0.0)
+    owed = Column(Float, default=0.0)
+    address = Column(String(100), default='')
+
+    def __str__(self):
+        return '<uid={0}, phone_number={1}>'.format(self.uid, self.phone_number)
 
     @classmethod
-    def add_new_user(cls, phone_number):
+    def create_new_user(cls, phone_number):
         user = User(phone_number=phone_number)
         session.add(user)
         session.commit()
+        user.uid = str(user.id).rjust(10, '0')
+        session.commit()
+        Access.create_new_access(user.uid)
         return user
 
     @classmethod
     def get_user_by_phone_number(cls, phone_number):
         return session.query(cls).filter_by(phone_number=phone_number).first()
 
+    @classmethod
+    def get_user_by_uid(cls, uid):
+        return session.query(cls).filter_by(uid=uid).first()
+
 
 class Access(Base):
-    KH = 0b1000
-    KF = 0b0100
-    JL = 0b0010
-    GM = 0b0001
-
     __tablename__ = 'access'
     uid = Column(CHAR(20), ForeignKey('user.uid'), primary_key=True)
     right = Column(Integer, default=KH)
     password = Column(String(500))
     token = Column(String(24))
 
+    def __str__(self):
+        return '<uid={0} right={1}>'.format(self.uid, bin(self.right))
+
+    @classmethod
+    def create_new_access(cls, uid):
+        access = cls(uid=uid)
+        session.add(access)
+        session.commit()
+        return access
+
+    @classmethod
+    def get_right_by_uid(cls, uid):
+        return session.query(cls).filter_by(uid=uid).first().right
+
 
 class UserFlow(Base):
-    ZC = 0b0001
-    RZ = 0b0010
-    SQ = 0b1000
-    SP = 0b0100
-
     __tablename__ = 'user_flow'
     id = Column(Integer, primary_key=True)
     start_uid = Column(CHAR(20), ForeignKey('user.uid'), nullable=False)
@@ -62,13 +92,23 @@ class UserFlow(Base):
     op = Column(Integer, nullable=False, default=0)
     datetime = Column(DateTime, nullable=False)
     brief = Column(String(100))
+    # extra_data = Column(CLOB)
+
+    def __str__(self):
+        return '<uid={0} op={1}>'.format(self.start_uid, bin(self.op))
+
+    @classmethod
+    def create_new_flow(cls, uid, op, extra_data, target_uid=None):
+        now = datetime.now()
+        brief = ""
+        user_flow = UserFlow(start_uid=uid, next_uid=target_uid, op=op, datetime=now, brief=brief)
+        session.add(user_flow)
+        session.commit()
+        return user_flow
 
 
-class UserLoads(Base):
-    HK = 0b0001
-    JK = 0b0010
-
-    __tablename__ = 'user_loads'
+class UserLoans(Base):
+    __tablename__ = 'user_loans'
     id = Column(Integer, primary_key=True)
     uid = Column(CHAR(20), ForeignKey('user.uid'), nullable=False)
     op = Column(Integer)
@@ -77,15 +117,24 @@ class UserLoads(Base):
     max_loads = Column(Float)
     remain_loads = Column(Float)
 
+    def __str__(self):
+        return '<uid={0} op={1} loads={2}>'.format(self.uid, bin(self.op), str(self.loads))
 
-def init():
-    engine = create_engine('sqlite:///test.db')
+
+def drop_all():
     Base.metadata.drop_all(engine)
+
+
+def create_all():
     Base.metadata.create_all(engine)
-    User.add_new_user('18723121647')
 
 
 if __name__ == '__main__':
-    init()
-    u = User.get_user_by_phone_number('18723121647')
-    print(u, u.id, u.phone_number)
+    # drop_all()
+    # create_all()
+    u = User.get_user_by_phone_number('18723121650')
+    print(u.id, u.uid, u.phone_number)
+    aright = Access.get_right_by_uid(u.uid)
+    print(aright)
+    uf = UserFlow.create_new_flow(u.uid, SQ, '')
+    print(uf)
