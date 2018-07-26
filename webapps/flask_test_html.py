044b57097b63a6b1d7c3ed2df052f3e3000eca68
MrEighth <mazhuang1994@163.com>
2018-07-26 21:57:05 +0800

    登录页前端
    前端

--- webapps/flask_test_html.py
+++ webapps/flask_test_html.py
@@ -1,11 +1,18 @@
 
 from flask import Flask, render_template
-
+from datetime import timedelta
 app = Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
 
 @app.route('/')
 def index():
     return render_template('test.html')
 
+@app.route('/<name>')
+def about(name):
+    return render_template("{}.html".format(name))
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="1994", debug=True);l
+    app.jinja_env.auto_reload = True 
+    app.run(host="0.0.0.0", port=1994, debug=True);
